325c1435286277e4cb3839f0122f68790e2ce1e5
Justin Wright <djw6497@gmail.com>
2023-11-06 17:52:33 -0600

    Completed GA4 pushing to turn in and demo.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 --Part 1
-
+--id: Int, name: VARCHAR
 --Part 2
-
+select name from employer where location = "St. Louis City";
 --Part 3
-
+drop table job;
 --Part 4
+select * from skill left join job_skills on skill.id = job_skills.skills_id where job_skills.jobs_id is not null order by name asc;
